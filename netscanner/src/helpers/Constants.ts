8ec829c3246aea41881ddf5422373275dfb6fa34
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-10-22 11:02:36 +0200

    Create Constants.ts

---
+++ netscanner/src/helpers/Constants.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_TIMEOUT: number = 5000;
+export const DEFAULT_TRIES: number = 5;
+export const DEFAULT_SIZE: number = 16;
+export const DEFAULT_MAX_HOPS: number = -1;
+export const DEFAULT_SHOW_HOSTNAMES: boolean = true;
+export const DEFAULT_HIDE_OFFLINE: boolean = true;
+export const DEFAULT_SHOW_PING_TIME: boolean = true;
+export const DEFAULT_SHOW_PORT_SCAN_TIME: boolean = true;
+export const DEFAULT_SHOW_TOTAL_TIME: boolean = true;
