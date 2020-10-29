3b3c9f1c9e522998260231413e400b047e8c4328
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-10-29 15:40:47 +0100

    Add gui implementation

--- netscanner/src/App.tsx
+++ netscanner/src/App.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import Descriptor from './components/descriptor/Descriptor';
+import TopNav from './components/top-nav/TopNav';
+import Form, { IField, IFieldType } from './components/form/Form';
 
-const App = () => <div> nettoscanneru </div>;
+const fields: Array<IField> = [
+  {name: "Subnets range", type: IFieldType.RANGE, placeholder: "Subnet"}
+];
+
+const App = () =>
+  <div>
+    <TopNav />
+      <Descriptor text={"Detect connected machines and open ports on networks"} />
+    <Form {...fields}/>
+  </div>;
 
 export default hot(module)(App);
