009a615f1fc696fb970323ef9dddb40859da8ca4
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-11-12 15:35:06 +0100

    Update Pinger.ts

--- netscanner/src/models/Pinger.ts
+++ netscanner/src/models/Pinger.ts
@@ -1,16 +1,16 @@
-import { Machine } from '../interfaces/Machine'
-import { NetOptions } from '../interfaces/NetOptions';
+import { IMachine } from '../interfaces/IMachine'
+import { INetOptions } from '../interfaces/INetOptions';
 import { IP } from "./IP";
 import * as ping from 'ping';
 
 export class Pinger {
 
-  public static async ping(host: IP, options?: NetOptions): Promise<Machine> {
-    return new Promise<Machine>((resolve, reject) => {
+  public static async ping(host: IP, options?: INetOptions): Promise<IMachine> {
+    return new Promise<IMachine>((resolve, reject) => {
       ping.promise.probe(host.toString(), {
         timeout: options.timeout / 1000.0
       }).then((r: ping.PingResponse) => {
-        let machine: Machine = {
+        let machine: IMachine = {
           online: r.alive,
           ip: host,
           hostname: r.host
