b18470a4d9747bdea9e0a7753841f625b0fcc363
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-11-12 15:32:00 +0100

    Fix Form fields input prop

--- netscanner/src/App.tsx
+++ netscanner/src/App.tsx
@@ -2,17 +2,21 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader';
 import Descriptor from './components/descriptor/Descriptor';
 import TopNav from './components/top-nav/TopNav';
-import Form, { IField, IFieldType } from './components/form/Form';
+import Form from './components/form/Form';
+import { IField } from "./interfaces/IField";
+import { FieldType } from "./enums/FieldType";
+
+
 
 const fields: Array<IField> = [
-  {name: "Subnets range", type: IFieldType.RANGE, placeholder: "Subnet"}
+  { name: "Subnets range", type: FieldType.RANGE, placeholder: "Subnet" }
 ];
 
 const App = () =>
-  <div>
+  <div className="app-content">
     <TopNav />
-      <Descriptor text={"Detect connected machines and open ports on networks"} />
-    <Form {...fields}/>
+    <Descriptor text={"Detect connected machines and open ports on networks"} />
+    <Form fields={fields} />
   </div>;
 
 export default hot(module)(App);
