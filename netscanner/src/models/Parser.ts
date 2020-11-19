8de2c3b0ef93eb063d61aa0773df9a1535aa96f7
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-11-19 15:26:56 +0100

    Update now it actually parses lul

--- netscanner/src/models/Parser.ts
+++ netscanner/src/models/Parser.ts
@@ -1,9 +1,24 @@
+import { Range } from './Range';
+
 const SPLIT_CHAR: string = ',';
-//const RANGE_SPLIT: string = '-';
+const RANGE_SPLIT: string = '-';
 //const RANGE_START: string = '[';
-//const RANGE_END: string = ']'
+const RANGE_END: string = ']'
 //const EXCLUDE_CHAR: string = '\\';
 
+/**
+ * Parses a custom expression that defines which ports to use.
+ * Rulesets: 
+ * `Range   -> [min-max]
+ * Add     -> ,
+ * Single  -> portnumber
+ * Exclude -> \
+ * Example: [1-10],20,\[6-10]
+ * Result:  [1,2,3,4,5,20]`
+ * 
+ * @author Ismael Trentin
+ * @version 2020.11.12
+ */
 export class Parser {
 
   private static cleanSplit(expression: string): Array<string> {
@@ -19,6 +34,10 @@ export class Parser {
   private static isPort(expression: string): boolean {
     let portTest: RegExp = /^\\{0,1}[0-9]{1,5}$/;
     return portTest.test(expression);
+  }
+
+  private static isExcluded(exp: string): boolean {
+    return exp.includes('\\');
   }
 
   private static isValid(expression: string): boolean {
@@ -38,19 +57,46 @@ export class Parser {
     return false;
   }
 
+  private static parseRange(range: string): Array<number> {
+    let min: number = parseInt(range.substring(1, range.indexOf(RANGE_SPLIT)));
+    let max: number = parseInt(range.substring(range.indexOf(RANGE_SPLIT) + 1, range.indexOf(RANGE_END)));
+    return Range.from(min, max);
+  }
+
+  private static parseExcludedRange(exRange: string): Array<number> {
+    return Parser.parseRange(exRange.substr(1));
+  }
+
+  /**
+   * Returns an array containing the ports defined by the expression.
+   * 
+   * @param expression the expression used to define the ports
+   */
   public static parse(expression: string): Array<number> {
     if (Parser.isValid(expression)) {
       let out: Array<number> = [];
       let splitted: Array<string> = Parser.cleanSplit(expression);
       for (let i: number = 0; i < splitted.length; i++) {
         let s: string = splitted[i];
-        if (Parser.isRange(s)) {
-
-        } else if (Parser.isPort(s)) {
-
+        if (this.isExcluded(s)) {
+          if (Parser.isRange(s)) {
+            out = out.sort((a: number, b: number) => a - b);
+            let range: Array<number> = Parser.parseExcludedRange(s);
+            out = out.filter((v: number) => !range.includes(v));
+          } else if (Parser.isPort(s)) {
+            let port: number = parseInt(s.substr(1));
+            out = out.filter((v: number) => v != port);
+          }
+        } else {
+          if (Parser.isRange(s)) {
+            let range: Array<number> = Parser.parseRange(s);
+            out = out.concat(range);
+          } else if (Parser.isPort(s)) {
+            out.push(parseInt(s));
+          }
         }
       }
-      return [];
+      return out;
     } else {
       throw 'Invalid pattern';
     }
