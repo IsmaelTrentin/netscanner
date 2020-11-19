26c4fbd5f1fdca2b7861e2aad4d1a717b8d70434
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-11-19 15:26:32 +0100

    Add new fields

--- netscanner/src/interfaces/IField.ts
+++ netscanner/src/interfaces/IField.ts
@@ -1,7 +1,16 @@
 import { FieldType } from '../enums/FieldType';
 
 export interface IField {
+
   name: string;
-  type?: FieldType
+  
+  type?: FieldType;
+  
+  inputType?: 'text' | 'number';
+  
   placeholder?: string;
+
+  span?: boolean;
+
+  value?: string | { start: number | undefined, end: number | undefined };
 }
