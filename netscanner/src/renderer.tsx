b8d52c4681d8e9ccb7439d7d21f70a87b71cd7fe
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-11-19 15:23:23 +0100

    Update to Router structure

--- netscanner/src/renderer.tsx
+++ netscanner/src/renderer.tsx
@@ -1,6 +1,14 @@
-import './index.css';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import { HashRouter, Link, Route } from 'react-router-dom';
 import App from './App';
+import ReportPage from './components/report-page/ReportPage';
+import './index.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+  <HashRouter>
+    <Route exact path="/" component={App} />
+    <Route exact path="/report" component={ReportPage} />
+  </HashRouter>,
+  document.getElementById('root')
+);
