3af37f1f4aca27d10da4b7fb6a4c61566ebe8d4e
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-11-19 15:24:50 +0100

    Add data binding

--- netscanner/src/components/form/Form.tsx
+++ netscanner/src/components/form/Form.tsx
@@ -3,17 +3,25 @@ import { hot } from 'react-hot-loader';
 import { IField } from './../../interfaces/IField';
 import FormField from './FormField';
 
-class Form extends React.Component<{ fields: Array<IField> }> {
+class Form extends React.Component<{ fields: Array<IField>, onChangeField: Function }> {
 
-  constructor(props: { fields: Array<IField> }) {
+  constructor(props: { fields: Array<IField>, onChangeField: Function }) {
     super(props);
+  }
+
+  onChange = (field: IField, value: { value: string } | { value: { start: number | undefined, end: number | undefined } }) => {
+    this.props.onChangeField(field, value.value);
   }
 
   render() {
     return (
       <div className="form">
         {Array.from(this.props.fields).map((f, i) => {
-          return <FormField key={i} name={f.name} placeholder={f.placeholder} type={f.type} />
+          return <FormField
+            key={i}
+            field={f}
+            onChange={this.onChange}
+          />
         })}
       </div>
     );
