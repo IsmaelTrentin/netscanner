d3e18fc122bd77e19f286e01c138a98bf3bc3a3a
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-11-19 15:22:31 +0100

    Update to Router structure

--- netscanner/src/App.tsx
+++ netscanner/src/App.tsx
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
-import Descriptor from './components/descriptor/Descriptor';
-import TopNav from './components/top-nav/TopNav';
-import Form from './components/form/Form';
-import { IField } from "./interfaces/IField";
-import { FieldType } from "./enums/FieldType";
+import ScanPage from './components/scan-page/ScanPage';
 
-
-
-const fields: Array<IField> = [
-  { name: "Subnets range", type: FieldType.RANGE, placeholder: "Subnet" }
-];
-
-const App = () =>
-  <div className="app-content">
-    <TopNav />
-    <Descriptor text={"Detect connected machines and open ports on networks"} />
-    <Form fields={fields} />
-  </div>;
-
+class App extends React.Component {
+  render() {
+    return (
+      <ScanPage />
+    );
+  }
+}
 export default hot(module)(App);
