147a15589c22f45fb212f215161e089b4b4edd10
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-12-03 15:39:38 +0100

    Add tabindex logic

--- netscanner/src/components/form/Form.tsx
+++ netscanner/src/components/form/Form.tsx
@@ -1,26 +1,32 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import { FieldType } from './../../enums/FieldType';
 import { IField } from './../../interfaces/IField';
 import FormField from './FormField';
 
-class Form extends React.Component<{ fields: Array<IField>, onChangeField: Function }> {
+class Form extends React.Component<{ fields: Array<IField<FieldType>>, onChangeField: Function }> {
 
-  constructor(props: { fields: Array<IField>, onChangeField: Function }) {
+  constructor(props: { fields: Array<IField<FieldType>>, onChangeField: Function }) {
     super(props);
   }
 
-  onChange = (field: IField, value: { value: string } | { value: { start: number | undefined, end: number | undefined } }) => {
+  onChange = (field: IField<FieldType>, value: { value: string } | { value: { start: number | undefined, end: number | undefined } }) => {
     this.props.onChangeField(field, value.value);
   }
 
   render() {
+    let index: number = 0;
+    let inc: number = 0;
     return (
       <div className="form">
         {Array.from(this.props.fields).map((f, i) => {
+          index = i + inc;
+          inc = 1;
           return <FormField
             key={i}
             field={f}
             onChange={this.onChange}
+            tabIndex={index}
           />
         })}
       </div>
