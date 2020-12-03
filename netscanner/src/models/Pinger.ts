a8d85dee244740065e24dea1c93d8f48e741e95a
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-12-03 15:42:50 +0100

    Fix timeout when options is undefined

--- netscanner/src/models/Pinger.ts
+++ netscanner/src/models/Pinger.ts
@@ -2,13 +2,14 @@ import { IMachine } from '../interfaces/IMachine'
 import { INetOptions } from '../interfaces/INetOptions';
 import { IP } from "./IP";
 import * as ping from 'ping';
+import * as Constants from './../helpers/Constants'; 
 
 export class Pinger {
 
   public static async ping(host: IP, options?: INetOptions): Promise<IMachine> {
     return new Promise<IMachine>((resolve, reject) => {
       ping.promise.probe(host.toString(), {
-        timeout: options.timeout / 1000.0
+        timeout: (options) ? options.timeout / 1000.0 : Constants.DEFAULT_TIMEOUT
       }).then((r: ping.PingResponse) => {
         let machine: IMachine = {
           online: r.alive,
