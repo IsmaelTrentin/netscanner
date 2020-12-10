670c5567e931c72270f985da04fd32fe7c835868
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-12-10 15:37:02 +0100

    Update switched functions to fields

--- netscanner/src/helpers/OptionsDefaults.ts
+++ netscanner/src/helpers/OptionsDefaults.ts
@@ -4,22 +4,19 @@ import * as Constants from './Constants';
 
 export class OptionsDefaults {
 
-  public static netOptionsDefaults(): INetOptions {
-    return {
-      timeout: Constants.DEFAULT_TIMEOUT,
-      tries: Constants.DEFAULT_TRIES,
-      size: Constants.DEFAULT_SIZE,
-      maxHops: Constants.DEFAULT_MAX_HOPS
-    };
+  public static readonly NET_OPTIONS_DEFAULT: INetOptions = {
+    timeout: Constants.DEFAULT_TIMEOUT,
+    tries: Constants.DEFAULT_TRIES,
+    size: Constants.DEFAULT_SIZE,
+    maxHops: Constants.DEFAULT_MAX_HOPS
   }
 
-  public static reportOptionsDefaults(): IReportOptions {
-    return {
-      hostnames: Constants.DEFAULT_SHOW_HOSTNAMES,
-      hideOffline: Constants.DEFAULT_HIDE_OFFLINE,
-      pingTime: Constants.DEFAULT_SHOW_PING_TIME,
-      portScanTime: Constants.DEFAULT_SHOW_PORT_SCAN_TIME,
-      totalTime: Constants.DEFAULT_SHOW_TOTAL_TIME
-    };
+  public static readonly REPORT_OPTIONS_DEFAULT: IReportOptions = {
+    fileName: Constants.DEFAULT_REPORT_FILE_NAME,
+    filePath: Constants.DEFAULT_REPORT_FILE_PATH,
+    hostnames: Constants.DEFAULT_SHOW_HOSTNAMES,
+    hideOffline: Constants.DEFAULT_HIDE_OFFLINE,
+    pingTime: Constants.DEFAULT_SHOW_PING_TIME,
+    totalTime: Constants.DEFAULT_SHOW_TOTAL_TIME
   }
 }
