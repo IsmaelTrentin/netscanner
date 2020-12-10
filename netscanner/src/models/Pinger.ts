ba84a3a90fe89c09002c8467fa550801958213d1
Ismael <61277905+IsmaelTrentin@users.noreply.github.com>
2020-12-10 15:38:56 +0100

    Update Pinger

--- netscanner/src/models/Pinger.ts
+++ netscanner/src/models/Pinger.ts
@@ -2,23 +2,41 @@ import { IMachine } from '../interfaces/IMachine'
 import { INetOptions } from '../interfaces/INetOptions';
 import { IP } from "./IP";
 import * as ping from 'ping';
-import * as Constants from './../helpers/Constants'; 
+import * as Constants from './../helpers/Constants';
+import { IScanResult } from './../interfaces/IScanResult';
 
 export class Pinger {
 
-  public static async ping(host: IP, options?: INetOptions): Promise<IMachine> {
-    return new Promise<IMachine>((resolve, reject) => {
+  public static async ping(host: IP, options?: INetOptions): Promise<IScanResult> {
+    return new Promise<IScanResult>((resolve, reject) => {
       ping.promise.probe(host.toString(), {
-        timeout: (options) ? options.timeout / 1000.0 : Constants.DEFAULT_TIMEOUT
+        timeout: (options) ? options.timeout / 1000.0 : Constants.DEFAULT_TIMEOUT / 1000.0
       }).then((r: ping.PingResponse) => {
         let machine: IMachine = {
           online: r.alive,
-          ip: host,
-          hostname: r.host
+          ip: new IP(r.numeric_host),
+          hostname: r.host,
+          openPorts: []
         };
-        resolve(machine);
+        let time: number = parseInt(r.time as string);
+        let result: IScanResult = {
+          machine: machine,
+          pingTime: (isNaN(time)) ? undefined : time
+        }
+        resolve(result);
         return;
       }).catch((err: any) => reject(err));
     });
   }
+
+  // public static async pingRange(ipStart: IP, ipEnd: IP, options?: INetOptions): Promise<IMachine[]> {
+  //   return new Promise<IMachine[]>(async (resolve, reject) => {
+  //     let results: Array<IMachine> = [];
+  //     while (ipStart.triplets != ipEnd.triplets) {
+  //       results.push(await Pinger.ping(ipStart, options));
+  //       ipStart.addT(0, 1);
+  //     }
+  //     return results;
+  //   });
+  // }
 }
