2ab7a94950414e7ab4bceec892ac9be9acf8a232
Ismael Trentin <ismael.trentin@samtrevano.ch>
2020-12-17 23:19:09 +0100

    Add JSDoc

--- netscanner/src/components/expand-btn/ExpandButton.tsx
+++ netscanner/src/components/expand-btn/ExpandButton.tsx
@@ -1,17 +1,46 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
 
+/**
+ * The ExpandButton component's props.
+ * 
+ * @author Ismael Trentin
+ * @version 2020.12.03
+ */
 export interface ExpandButtonProps {
+
+  /**
+   * Called when the button is clicked.
+   */
   onClick: Function;
+
+  /**
+   * The text contained in the button.
+   */
   text: string;
 }
 
+/**
+ * A simple button used for displaying additional
+ * parameters to be passed in the scan page.
+ * 
+ * @author Ismael Trentin
+ * @version 2020.12.03
+ */
 class ExpandButton extends React.Component<ExpandButtonProps>{
 
+  /**
+   * Instantiates a new ExpandButton component.
+   * 
+   * @param props the component's props
+   */
   constructor(props: ExpandButtonProps) {
     super(props);
   }
 
+  /**
+   * Called when the button is clicked.
+   */
   onClick = () => {
     this.props.onClick();
   }
