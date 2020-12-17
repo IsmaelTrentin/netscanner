0564f5faeef07b9145cbc30a7c3d0ee5d7f89f9a
Ismael Trentin <ismael.trentin@samtrevano.ch>
2020-12-17 23:20:18 +0100

    Add JSDoc

--- netscanner/src/components/top-nav/TopNav.tsx
+++ netscanner/src/components/top-nav/TopNav.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
+/**
+ * The top dropdown menu component.
+ * 
+ * @author Ismael Trentin
+ * @version 2020.12.10
+ */
 class TopNav extends React.Component {
+
   render() {
     return (
       <div>
@@ -10,7 +17,8 @@ class TopNav extends React.Component {
           <p className="title">Net Scanner</p>
         </div>
         <div className="dropdown">
-          <img src="" alt="X" className="dropbtn" />
+          {/* <img src="" alt="Menu" className="dropbtn" /> */}
+          <p>MENU</p>
           <div className="dropdown-content">
             <Link to="/">Scan</Link>
             <Link to="/scan">Report</Link>
