2df37ff0fb879a4c47fc162a93611c7a82c6653d
Ismael Trentin <ismael.trentin@samtrevano.ch>
2020-12-17 23:18:58 +0100

    Add JSDoc

--- netscanner/src/components/about-page/AboutPage.tsx
+++ netscanner/src/components/about-page/AboutPage.tsx
@@ -1,19 +1,60 @@
-import { hot } from "react-hot-loader";
 import * as React from 'react';
-import Descriptor from "../descriptor/Descriptor";
-import TopNav from "../top-nav/TopNav";
+import { hot } from 'react-hot-loader';
 
+import Descriptor from '../descriptor/Descriptor';
+import TopNav from '../top-nav/TopNav';
+
+/**
+ * Defines the AboutPage component's props.
+ * 
+ * @author Ismael Trentin
+ * @version 2020.12.10
+ */
 interface AboutPageState {
+
+  /**
+   * The software version.
+   */
   version: string;
+
+  /**
+   * The author's full name.
+   */
   author: string;
+
+  /**
+   * The port expression tutorial.
+   */
   expTutorial: {
+
+    /**
+     * The range example.
+     */
     range: string,
+
+    /**
+     * The port example.
+     */
     port: string,
+
+    /**
+     * The exclude example.
+     */
     exclude: string,
+
+    /**
+     * The full example.
+     */
     example: string
   };
 }
 
+/**
+ * The AboutPage page component.
+ * 
+ * @author Ismael Trentin
+ * @version 2020.12.10
+ */
 class AboutPage extends React.Component<{}, AboutPageState> {
 
   constructor(props: {}) {
